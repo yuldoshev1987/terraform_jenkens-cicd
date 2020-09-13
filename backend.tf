b0bdd658761dc3633a4415451bdbbb503c800198
Rustam <yuldoshev1987@gmail.com>
2020-09-12 21:33:16 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "myterraform-state-2020"
+    bucket = "myterraform-state-2022"
     key = "terraform.tfstate-development"
     region = "us-east-1"
     dynamodb_table = "terraformstatelock"
